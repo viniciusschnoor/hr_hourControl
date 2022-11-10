5ffdc13d298bf9a7c136c6038d161bc857670535
Vinícius Schnoor <72426583+viniciusschnoor@users.noreply.github.com>
2022-11-10 15:35:15 -0300

    FIRST STEP
    > Ignore confidential.py that contains confidential data
    > Created flow to create folders and files needed to run the script
    > Uploaded schema that contains central idea about application

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # hr_hourControl
  
+ <b>Developed by: </b>Vinicius Schnoor<br>
+ <b>Version: </b>1.0 BETA<br>
+ <b>Release: </b>
+ <h2>Description</h2>
+ <h3>SCHEMA</h3>
+ <img src='SCHEMA.png' style='background-color:#fff; padding: 20px; border-radius: 10px;'>
+ <hr>
+ <p>To start, do you need create a virtual enviroment. At command prompt, type:</p>
+ <code>
+ py -m venv venv<br>
+ .\venv\Scripts\activate<br>
+ py -m pip install --upgrade pip
+ </code>
---
+++ main.py
@@ -0,0 +1,44 @@
+from confidential import credential
+import getpass
+import os # Verify directory and files
+
+
+# - Start of confidential.py file ---------------------------------- #
+# It was defined at confidential.py file to hidden confidential data
+
+# def credential(user):
+#     root = fr'C:\Users\{user}\[...]\Documents\hr_HourControl'
+#     base = fr'{root}\database.csv'
+#     return {'root': root, 'base': base}
+
+# - End of confidential.py file ------------------------------------ #
+
+
+# VARIABLES
+user = credential(getpass.getuser())
+root_verify = os.path.exists(user['root'])
+base_verify = os.path.exists(user['base'])
+sys_verify = [root_verify, base_verify]
+
+# FUNCTIONS
+def create_structure(id):
+    if id == 0:
+        os.makedirs(user['root'])
+    database = open(user['base'],'w+')
+    header = 'id,enter_hour,pause_hour,return_hour,exit_hour,first_dur,pause_dur,second_dur,total_dur,extra_time'
+    first_content = list()
+    first_content.append(header)
+    first_content.append('\n1,,,,,,,,,')
+    database.writelines(first_content)
+    database.close()
+
+# PROCESS
+match sys_verify:
+    case [True, True]:
+        pass
+    case [True, False]:
+        create_structure(1)
+    case _:
+        create_structure(0)
+
+print('FIM')
